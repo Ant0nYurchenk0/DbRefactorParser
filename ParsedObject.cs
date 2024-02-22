92456916b483bb3cf373b9aade5e2721b78fa09a
Ant0nYurchenk0 <an.yurchenko.an@gmail.com>
2024-02-22 12:59:31 +0200

    first commit

---
+++ ParsedObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbRefactorParser
+{
+    internal class ParsedObject
+    {
+        public string RecordName { get; set; }
+        public string SetName { get; set; }
+        public List<string> FieldsList { get; set; } = new();
+        public override string ToString()
+        {
+            var result = new StringBuilder();
+            result.Append("{");
+            result.Append($"\n\trecord-name: \"{RecordName}\"");
+            result.Append($"\n\tset-name: \"{SetName}\"");
+            if(FieldsList.Count > 0)
+            {
+                result.Append($"\n\tfields-list:");
+                result.Append($"\n\t[");
+                foreach ( var field in FieldsList )
+                {
+                    result.Append($"\n\t\t\"{field}\"");
+                }
+                result.Append($"\n\t]");
+            }
+            result.Append("\n}");
+            return result.ToString();   
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+
+namespace DbRefactorParser
+{
+//find-st3 ::= “Find “ [“Next ” |”First “ |”Prior “ |”Last “] rec-name => ident 
+//“Record “ “In “ set-name =>ident “Set “ 
+//[“Using ” field-list => list-ident]! “;” ;
+
+//list-ident ::= [item-name => ident]+ “;” ;
+    internal class Program
+    {
+        static void Main()
+        {
+            var sr = new StreamReader("D:\\Prog\\C#\\DbRefactorParser\\input2.txt");
+            var line = sr.ReadToEnd();
+            var regex = new Regex(@"Find\s+(?:Next|First|Prior|Last)\s+(\w+)\s+Record\s+In\s+(\w+)\s+Set\s*(?:\sUsing\s+(.+)){0,1}\s*;");
+            var matches = regex.Matches(line);
+            var obj = new ParsedObject();
+            if (matches == null || matches.Count == 0) throw new Exception("No match found");
+            var groups = matches.First().Groups;
+            if (matches != null && matches.First().Groups.Count>2) 
+            {
+                obj.RecordName = groups[1].Value;
+                obj.SetName = groups[2].Value;
+                if(groups.Count > 3)
+                {
+                    var regex1 = new Regex(@"(\w+)");
+                    var matches1 = regex1.Matches(groups[3].Value);
+                    for (int i = 0; i < matches1.Count; i++)
+                    {
+                        obj.FieldsList.Add(matches1[i].Value);
+                    }
+                }
+            }
+            var objStr = obj.ToString();
+            using (StreamWriter writer = new StreamWriter("D:\\Prog\\C#\\DbRefactorParser\\output.txt", false))
+            {
+                writer.Write(objStr);
+            }
+        }
+    }
+}
